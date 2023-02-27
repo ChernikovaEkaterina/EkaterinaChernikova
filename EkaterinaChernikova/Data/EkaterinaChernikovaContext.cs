0d2c5ae7f0f35ac5cb1d3a16c397c134a17b42b7
Черникова Екатерина <92220708+ChernikovaEkaterina@users.noreply.github.com>
2023-02-27 18:40:31 +0300

    Добавьте файлы проекта.

---
+++ EkaterinaChernikova/Data/EkaterinaChernikovaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EkaterinaChernikova.Models;
+
+namespace EkaterinaChernikova.Data
+{
+    public class EkaterinaChernikovaContext : DbContext
+    {
+        public EkaterinaChernikovaContext(DbContextOptions<EkaterinaChernikovaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EkaterinaChernikova.Models.Movie> Movie { get; set; }
+    }
+}
